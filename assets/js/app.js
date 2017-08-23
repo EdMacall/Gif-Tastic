99f1ba46bd8370f5cfc35431be7893a5eb642f33
EdMacall <irvedwmac@gmail.com>
2017-08-23 03:42:19 -0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret",
+               "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat",
+               "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
+
+var buttonsDiv = $("<div>");
+
+console.log(animals);
+
+function init(){
+  var animalsLength = animals.length;
+  for(var i = 0; i < animalsLength; i++){
+    // animals[i];
+  }
+};
+
+// submit button
+/* $().on("click", function(){
+  animals.push("textFieldValue");
+}); */
+
+// gif button animate
+/* $().on("click", function(){
+	if(something === "still"){
+	  setto = "animated";
+	  set("attr", "animated");
+	}
+	else{
+	  setto = "still";
+	  set("attr", "still");
+	}
+}); */
+
+init();
+
